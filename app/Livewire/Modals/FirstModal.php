e1e5f60cc94f15ffb1365b87a78767652c94cb83
Alex Garrett-Smith <alex@codecourse.com>
2023-10-10 15:02:34 +0100

    Episode 06

--- app/Livewire/Modals/FirstModal.php
+++ app/Livewire/Modals/FirstModal.php
@@ -9,6 +9,17 @@ class FirstModal extends Component
 {
     use IsModal;
 
+    public $counter = 0;
+
+    public function increment()
+    {
+        $this->counter++;
+
+        if ($this->counter === 10) {
+            $this->dispatch('hide')->self();
+        }
+    }
+
     public function render()
     {
         return view('livewire.modals.first-modal');
--- resources/views/livewire/modals/first-modal.blade.php
+++ resources/views/livewire/modals/first-modal.blade.php
@@ -1,6 +1,10 @@
 <x-modal-wrapper wire:model="visible">
     <div>Modal content</div>
     <div>
+        {{ $counter }}
+        <button wire:click="increment">Increment</button>
+    </div>
+    <div>
         <button wire:click="dispatchTo('modals.second-modal', 'show')">Open second modal</button>
     </div>
 </x-modal-wrapper>
