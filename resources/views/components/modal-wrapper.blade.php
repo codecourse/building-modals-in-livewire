fbf18f9124ae44e7b602b2b5072dd2f148e7874e
Alex Garrett-Smith <alex@codecourse.com>
2023-10-10 14:37:53 +0100

    Episode 03

--- resources/views/components/modal-wrapper.blade.php
+++ resources/views/components/modal-wrapper.blade.php
@@ -2,14 +2,17 @@
     class="relative z-10"
     x-data="{ visible: @entangle($attributes->wire('model')) }"
     x-show="visible"
+    x-on:keydown.escape.window="visible = false"
 >
     <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
         <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-            <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+            <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" x-on:click="visible = false"></div>
 
             <div class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
                 <div>
                     {{ $slot }}
+
+                    <button x-on:click="visible = false">Close</button>
                 </div>
             </div>
         </div>
