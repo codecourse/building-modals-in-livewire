ea5ba5632a05a56d716b20751ac3981a5bf0706a
Alex Garrett-Smith <alex@codecourse.com>
2023-10-11 12:36:27 +0100

    Modal wrapper design

---
+++ resources/views/components/modal-wrapper-design.blade.php
@@ -0,0 +1,13 @@
+<div class="relative z-10">
+    <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+        <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+
+            <div class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+                <div>
+                    Modal content goes here!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
